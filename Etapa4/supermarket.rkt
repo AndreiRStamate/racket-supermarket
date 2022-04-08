381cae564a3073ca20c7ed2bc1405db8a5c8077e
Andrei R. Stamate <a144185i@gmail.com>
2022-04-08 20:47:52 +0300

    Create supermarket.rkt

---
+++ Etapa4/supermarket.rkt
@@ -0,0 +1,55 @@
+#lang racket
+(require racket/match)
+(require "queue.rkt")
+
+(provide (all-defined-out))
+
+(define ITEMS 5)
+
+
+; TODO
+; Aveți libertatea să vă structurați programul cum doriți (dar cu restricțiile
+; de mai jos), astfel încât funcția serve să funcționeze conform specificației.
+; 
+; Restricții (impuse de checker):
+; - trebuie să existe în continuare funcția (empty-counter index)
+; - cozile de la case trebuie implementate folosind noul TDA queue
+
+(define-struct counter (index tt et queue) #:transparent)
+
+(define (empty-counter index)
+  (counter index 0 0 empty-queue))
+  
+  
+; TODO
+; Implementați funcția care simulează fluxul clienților pe la case.
+; ATENȚIE: Față de etapa 3, apare un nou tip de cerere, așadar
+; requests conține 5 tipuri de cereri (cele moștenite din etapa 3 plus una nouă):
+;   - (<name> <n-items>) - persoana <name> trebuie așezată la coadă la o casă              (ca înainte)
+;   - (delay <index> <minutes>) - casa <index> este întârziată cu <minutes> minute         (ca înainte)
+;   - (ensure <average>) - cât timp tt-ul mediu al caselor este mai mare decât
+;                          <average>, se adaugă case fără restricții (case slow)           (ca înainte)
+;   - <x> - trec <x> minute de la ultima cerere, iar starea caselor se actualizează
+;           corespunzător (cu efect asupra câmpurilor tt, et, queue)                       (ca înainte)
+;   - (close <index>) - casa index este închisă                                            (   NOU!   )
+; Sistemul trebuie să proceseze cele 5 tipuri de cereri în ordine, astfel:
+; - persoanele vor fi distribuite la casele DESCHISE cu tt minim; nu se va întâmpla
+;   niciodată ca o persoană să nu poată fi distribuită la nicio casă                       (mică modificare)
+; - când o casă suferă o întârziere, tt-ul și et-ul ei cresc (chiar dacă nu are clienți);
+;   nu aplicați vreun tratament special caselor închise                                    (ca înainte)
+; - tt-ul mediu (ttmed) se calculează pentru toate casele DESCHISE, 
+;   iar la nevoie veți adăuga case slow una câte una, până când ttmed <= <average>         (mică modificare)
+; - când timpul prin sistem avansează cu <x> minute, tt-ul, et-ul și queue-ul tuturor 
+;   caselor se actualizează pentru a reflecta trecerea timpului; dacă unul sau mai mulți 
+;   clienți termină de stat la coadă, ieșirile lor sunt contorizate în ordine cronologică. (ca înainte)
+; - când o casă se închide, ea nu mai primește clienți noi; clienții care erau deja acolo
+;   avansează normal, până la ieșirea din supermarket                                    
+; Rezultatul funcției serve va fi o pereche cu punct între:
+; - lista sortată cronologic a clienților care au părăsit supermarketul:
+;   - fiecare element din listă va avea forma (index_casă . nume)
+;   - dacă mai mulți clienți ies simultan, sortați-i crescător după indexul casei
+; - lista cozilor (de la case DESCHISE sau ÎNCHISE) care încă au clienți:
+;   - fiecare element va avea forma (index_casă . coadă) (coada este de tip queue)
+;   - lista este sortată după indexul casei
+(define (serve requests fast-counters slow-counters)
+  'your-code-here)
